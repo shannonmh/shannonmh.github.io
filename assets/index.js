77291f226ef1fdef5adcd78cb6128ee27f9bf091
SMH <shannonmhenry1@gmail.com>
2019-02-04 02:19:59 -0600

    fixed bugs

--- assets/index.js
+++ assets/index.js
@@ -1,13 +1,19 @@
 new TypeIt('#type', {
-    speed: 100,
-    strings: ["Hello World", "Web Developer", "Designer", "Animator"],
-    deleteSpeed: null,
-    lifeLike: true,
-    cursor: true,
-    cursorSpeed: 1000,
-    startDelay: 250,
-    breakLines: false,
-    loop: true,
-    loopDelay: 1500,
-    nextStringDelay: 1500
-  }).go();
+  speed: 100,
+  strings: ["Hello World", "Web Developer", "Designer", "Animator"],
+  deleteSpeed: null,
+  lifeLike: true,
+  cursor: true,
+  cursorSpeed: 1000,
+  startDelay: 250,
+  breakLines: false,
+  loop: true,
+  loopDelay: 1500,
+  nextStringDelay: 1500
+}).go();
+
+$(".move-link").on("click", function () {
+  document.querySelector(".contact-nav").scrollIntoView({
+      behavior: "smooth"
+  });
+});

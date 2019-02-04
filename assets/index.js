bec0d1f0b130edc7720177d57402b46bcbc2cf1a
SMH <shannonmhenry1@gmail.com>
2019-02-03 23:57:25 -0600

    Updated changes

---
+++ assets/index.js
@@ -0,0 +1,13 @@
+new TypeIt('#type', {
+    speed: 100,
+    strings: ["Hello World", "Web Developer", "Designer", "Animator"],
+    deleteSpeed: null,
+    lifeLike: true,
+    cursor: true,
+    cursorSpeed: 1000,
+    startDelay: 250,
+    breakLines: false,
+    loop: true,
+    loopDelay: 1500,
+    nextStringDelay: 1500
+  }).go();
